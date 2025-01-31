d174fa665e0ae46f8dc689019f1e66b061c0c4c1
Chidvitha Yenumula <chidvitha125@gmail.com>
2025-01-31 15:48:20 -0700

    Added DRUD API for User

---
+++ crud/crud/src/main/java/com/example/crud/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.crud.service;
+
+import com.example.crud.model.User;
+import com.example.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        System.out.println("Saving user: " + user);
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public boolean deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
